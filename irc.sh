c7d1cc8b77d3d8d52974221e717a5bfb50e50de7
Tobias Hansson <me@tobiashansson.nu>
2013-04-20 20:44:05 +0200

    updated irc.sh wit description

--- irc.sh
+++ irc.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+###
+#Script class: silly & small!
+#Used to make the use of screen+irssi less scary for people that are afraid of terminals and ssh connections.
+###
 
 status=`screen -ls | grep -c "No Sockets"`
 if [ ${status} = "1" ]
